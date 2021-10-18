af9e47046b611c11c050705f6c8aba6d6839cf5f
BernardoRicci <90769942+BernardoRicci@users.noreply.github.com>
2021-10-18 10:55:12 +0200

    Update Complex.cc

--- Complex.cc
+++ Complex.cc
@@ -3,6 +3,7 @@
 #include "Complex.h"
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 //Constructors
@@ -44,10 +45,109 @@ double Complex::phi(){
     return atan(imm_/real_);
 }
 
+
+//Overloading between Complex
+Complex Complex::operator+ (const Complex& rhs) const {
+
+  double sum_re = real_ + rhs.real_;
+  double sum_im = imm_ + rhs.imm_;
+
+  return Complex(sum_re, sum_im);
+
+}
+
+Complex Complex::operator- (const Complex& rhs) const {
+
+  double diff_re = real_ - rhs.real_;
+  double diff_im = imm_ - rhs.imm_;
+
+  return Complex(diff_re, diff_im);
+
+}
+
+Complex Complex::operator* (const Complex& rhs) const {
+
+  double product_re = real_ * rhs.real_ - imm_ * rhs.imm_;
+  double product_im = real_ * rhs.imm_ + imm_ * rhs.real_;
+
+  return Complex(product_re, product_im);
+
+}
+
+Complex Complex::operator/ (const Complex& rhs) const {
+
+  double div_re = (real_ * rhs.real_ + imm_ * rhs.imm_) * (1/(rhs.real_*rhs.real_ + rhs.imm_*rhs.imm_));
+  double div_im = (real_ + rhs.imm_ - imm_ * rhs.real_) * (1/(rhs.real_*rhs.real_ + rhs.imm_*rhs.imm_));
+
+  return Complex(div_re, div_im);
+
+}
+
+const Complex& Complex::operator= (const Complex& rhs) {
+
+  real_ = rhs.real_;
+  imm_ = rhs.imm_;
+
+  return *this;
+
+}
+
+const Complex& Complex::operator+= (const Complex& rhs) {
+
+  real_ += rhs.real_;
+  imm_ += rhs.imm_;
+
+  return *this;
+
+}
+
+const Complex& Complex::operator-= (const Complex& rhs) {
+
+  real_ -= rhs.real_;
+  imm_ -= rhs.imm_;
+
+  return *this;
+
+}
+
+const Complex& Complex::operator*= (const Complex& rhs) {
+
+  double new_re = real_ * rhs.real_ - imm_ * rhs.imm_;
+  double new_im = real_ * rhs.imm_ + imm_ * rhs.real_;
+
+  real_ = new_re;
+  imm_ = new_im;
+
+  return *this;
+
+}
+
+const Complex& Complex::operator/= (const Complex& rhs) {
+
+  double new_re = (real_ * rhs.real_ + imm_ * rhs.imm_) * (1/(rhs.real_*rhs.real_ + rhs.imm_*rhs.imm_));
+  double new_im =  (real_ + rhs.imm_ - imm_ * rhs.real_) * (1/(rhs.real_*rhs.real_ + rhs.imm_*rhs.imm_));
+
+  real_ = new_re;
+  imm_ = new_im;
+  
+  return *this;
+
+}
+
 //Overloading between Complex & double
 Complex Complex::operator*( const double& rhs ) const {
     return Complex(real_*rhs, imm_*rhs);
 }
 Complex Complex::operator/( const double& rhs ) const {
     return Complex(real_/rhs, imm_/rhs);
+}
+
+//Global functions
+Complex operator*(const double& lhs, const Complex& rhs){
+  return Complex(lhs*rhs.real(), lhs*rhs.imm() );
+}
+ostream& operator<<(ostream& os, const Complex& rhs){
+  using namespace std;
+  os << rhs.real() << " +/- " << rhs.imm();
+  return os;
 }

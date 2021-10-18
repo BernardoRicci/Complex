73f950ff7d991516f95286d5f44dec04699bf981
BernardoRicci <90769942+BernardoRicci@users.noreply.github.com>
2021-10-18 09:07:49 +0200

    Create Complex.cc

---
+++ Complex.cc
@@ -0,0 +1,20 @@
+//Complex.cc
+
+#include "Complex.h"
+
+#include <iostream>
+using namespace std;
+
+//Constructors
+Complex::Complex(){
+    real_=0.;
+    imm_=0.;
+}
+Complex::Complex(double x, double y){
+    real_=x;
+    imm_=y;
+}
+Complex::Complex(const Complex& complex){
+    real_=complex.real_;
+    imm_=complex.imm_;
+}

5fb99a96b7630ebb417502b09a4971d2aa0ce3cf
BernardoRicci <90769942+BernardoRicci@users.noreply.github.com>
2021-10-18 10:54:33 +0200

    Update Complex.h

--- Complex.h
+++ Complex.h
@@ -3,6 +3,10 @@
 #ifndef Complex_h
 #define Complex_h
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
 class Complex{
     public:
     //Constructors & Destructor
@@ -11,12 +15,19 @@ class Complex{
     Complex(const Complex& complex);    //Copy constructor
     ~Complex() {};                      //Destructor
 
+    //Getters
+    double real() const {return real_;}
+    double imm() const {return imm_;}
+    double R() const {return sqrt( (real_*real_) + (imm_*imm_));}
+    double Phi() const {return atan(imm_/real_);}
+
     //Setters
     void SetReal(double real) { real_=real;};
     void SetImmaginary(double imm) { imm_=imm; };
-    
+
     //print
     void print() const;
+
 
     //Functions
     double re();
@@ -25,15 +36,32 @@ class Complex{
     double r();
     double phase();
     double phi();
-    
-    
+
+    //Overloading between Complex
+    Complex operator+ (const Complex& rhs) const; //SUM between two complex numbers
+    Complex operator- (const Complex& rhs) const; //DIFFERENCE between two complex numbers
+    Complex operator* (const Complex& rhs) const; //MOLTIPLICATION between two complex numbers
+    Complex operator/ (const Complex& rhs) const; //DIVISION between two complex numbers
+
+    const Complex& operator= (const Complex& rhs); //Assignment operator
+    const Complex& operator+= (const Complex& rhs); //Sum & Assignment operator
+    const Complex& operator-= (const Complex& rhs); //Difference & Assignment operator
+    const Complex& operator*= (const Complex& rhs); //Moltiplication & Assignment operator
+    const Complex& operator/= (const Complex& rhs); //Division & Assignment operator
+
     //Overloading between Complex & double
     Complex operator*( const double& rhs ) const;
     Complex operator/( const double& rhs ) const;
+
 
     private:
     double real_;
     double imm_;
 
 };
+
+//Global functioncs
+Complex operator*(const double& lhs, const Complex& rhs);
+ostream& operator<<(ostream& os, const Complex& rhs);
+
 #endif
